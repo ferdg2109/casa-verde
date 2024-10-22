c198ee95c05711e75d1d32d0faa001f2b786b100
EAhumada08 <emaahmc08@gmail.com>
2024-10-22 09:41:46 -0600

    agregar componentes, agregar rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { ColeccionesComponent } from './pages/colecciones/colecciones.component';
+import { EmpresaComponent } from './pages/empresa/empresa.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path:'', redirectTo: "home", pathMatch: 'full'},
     {path:'home', component: HomeComponent},
-    /*{path:'servicios' },
-    {path:'empresa'},
-    {path:'contactos'},
-    {path:'signup'},
-    {path:'login'},*/
+    {path:'servicios', component: ServiciosComponent },
+    {path:'colecciones', component: ColeccionesComponent},
+    {path:'empresa', component: EmpresaComponent},
+    {path:'contactos', component: ContactosComponent},
+    {path:'signup', component: SignupComponent},
+    {path:'login', component: LoginComponent},
 ];
---
+++ src/app/pages/colecciones/colecciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colecciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './colecciones.component.html',
+  styleUrl: './colecciones.component.css'
+})
+export class ColeccionesComponent {
+
+}
