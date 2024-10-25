fef6915b76dc94c75edcf5acc00e0f6aa7d1a53c
ferdg2109 <21690073@tecvalles.mx>
2024-10-25 15:10:48 -0600

    colecc

--- src/app/pages/colecciones/colecciones.component.ts
+++ src/app/pages/colecciones/colecciones.component.ts
@@ -16,4 +16,11 @@ export class ColeccionesComponent {
   img6 = 'images/coleccion/11.png'
   img7 = 'images/coleccion/18.png'
   img8 = 'images/coleccion/30.png'
+  img9 = 'images/coleccion/31.png'
+  img10 = 'images/coleccion/34.png'
+  img11 = 'images/coleccion/35.png'
+  img13 = 'images/coleccion/13.png'
+  img14 = 'images/coleccion/14.png'
+  img15 = 'images/coleccion/15.png'
+  img16 = 'images/coleccion/16.png'
 }
