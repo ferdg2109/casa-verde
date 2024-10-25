6876c019f6e487064268c212aafaf712f604b259
ferdg2109 <21690073@tecvalles.mx>
2024-10-24 23:42:18 -0600

    colecc

--- src/app/pages/colecciones/colecciones.component.ts
+++ src/app/pages/colecciones/colecciones.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './colecciones.component.css'
 })
 export class ColeccionesComponent {
-
+  img1 = 'images/coleccion/1.png'
+  img2 = 'images/coleccion/4.png'
+  img3 = 'images/coleccion/6.png'
+  img4 = 'images/coleccion/7.png'
+  img5 = 'images/coleccion/10.png'
+  img6 = 'images/coleccion/11.png'
+  img7 = 'images/coleccion/18.png'
+  img8 = 'images/coleccion/30.png'
 }
