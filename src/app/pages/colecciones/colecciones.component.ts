377ce8054be2cbdf848a0fe5ebd65a430798bc29
EAhumada08 <emaahmc08@gmail.com>
2024-10-24 19:21:44 -0600

    probar renderizado con datos locales

--- src/app/pages/colecciones/colecciones.component.ts
+++ src/app/pages/colecciones/colecciones.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import * as data from './sombreros.json'
+
+interface User {
+  nombre:string;    
+}
 
 @Component({
   selector: 'app-colecciones',
   standalone: true,
   imports: [],
-  templateUrl: './colecciones.component.html',
+  template: `
+    <h1>{{users.nombre}}</h1>
+  `,
   styleUrl: './colecciones.component.css'
 })
 export class ColeccionesComponent {
-
+  users: User = data;
 }
