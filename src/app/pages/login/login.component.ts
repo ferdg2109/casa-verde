e40d99134ed1ca0f701bcfac65463d995ae1443e
manuac777 <manuelacuna78398@gmail.com>
2024-10-26 22:11:38 -0600

    Login y SingUp

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../../app.routes';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule,RouterLink,RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  logo ="images/logo.png"
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado:', this.loginForm.value);
+    } else {
+      console.log('Formulario inválido');
+      this.loginForm.markAllAsTouched(); 
+    }
+  }
+  isInvalid(fieldName: string): boolean {
+    const control = this.loginForm.get(fieldName);
+    return !!control && control.invalid && (control.touched || control.dirty);
+  }
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule,RouterLink,RouterLinkActive],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css'] 
 })
 export class SignupComponent {
+  registerForm: FormGroup;
+  logo = "images/logo.png";
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      middleName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]], 
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario de registro enviado:', this.registerForm.value);
+    } else {
+      console.log('Formulario inválido');
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+  isInvalid(fieldName: string): boolean {
+    const control = this.registerForm.get(fieldName);
+    return !!control && control.invalid && (control.touched || control.dirty);
+  }
 }
